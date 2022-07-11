11dd5f24baaf0c2addff1e7bb66f324c5f2167f6
Lilly <68789766+Paisseon@users.noreply.github.com>
2022-07-11 21:20:00 +0200

    Add progress bar + credits view
    DispatchQueue 🙏

--- permasigneriOS/ButtonStyle.swift
+++ permasigneriOS/ButtonStyle.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct SignButtonStyle: ButtonStyle {
+    @StateObject var progress: Progress = .shared
+    
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .frame(minWidth: 0,maxWidth: .infinity)
             .foregroundColor(Color.white)
             .padding()
-            .background(Color.blue)
-            .cornerRadius(40)
+            .background(
+                GeometryReader { geo in
+                    Color.blue
+                        .opacity(0.5)
+                    Color.blue
+                        .frame(width: geo.size.width * progress.Percent)
+                        .animation(.easeIn)
+                }
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 40, style: .continuous))
             .padding()
             .scaleEffect(configuration.isPressed ? 0.8 : 1.0)
     }
