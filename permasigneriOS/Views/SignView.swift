c5f9ad69a76ca8bf28dfa9979cde59c8df3d4cc9
Lilly <68789766+Paisseon@users.noreply.github.com>
2022-07-13 00:54:28 +0200

    Fix permissions on unc0ver 8.0.2

--- permasigneriOS/Views/SignView.swift
+++ permasigneriOS/Views/SignView.swift
@@ -21,7 +21,7 @@ struct SignView: View {
     
     @State var showInFilzaAlert:Bool = false
     @State var canShowinFilza:Bool = false
-        
+    
     func signFailedAlert(title:String, message:String) {
         checkapp.fileName = ""
         alertTitle = title
@@ -29,83 +29,95 @@ struct SignView: View {
         showAlert.toggle()
     }
     
-    private func finish() {
-        DispatchQueue.main.asyncAfter(deadline: .now()) {
-            if progress.CheckDebBuild() {
-                if checkFilza() {
-                    canShowinFilza = true
-                } else { canShowinFilza = false }
-                checkapp.fileName = ""
-                showInFilzaAlert.toggle()
-                
-            } else {
-                signFailedAlert(title: "Sign Failed", message: "Please try others iPA files")
-            }
-        }
-    }
-    
     var body: some View {
-            VStack {
-                Text(checkapp.fileName != "" ? "\(checkapp.fileName)" : "No ipa file selected")
-                Button(action: {isImporting.toggle()}, label: {Text("Select File")})
-                    .alert(isPresented: $showAlert) {
-                        Alert(title: Text(alertTitle), message: Text(alertMeaasge), dismissButton: .default(Text("OK")))
-                    }
-                    .fileImporter(
-                        isPresented: $isImporting,
-                        allowedContentTypes: [UTType(filenameExtension: "ipa")!],
-                        allowsMultipleSelection: false
-                    ) { result in
-                        do {
-                            let fileUrl = try result.get()
-                            checkapp.fileName = fileUrl.first!.lastPathComponent
-                            checkapp.filePath = fileUrl.first!.path
-                            
-                            checkapp.extractIpa()
-                            
-                            if checkapp.checkIsIpaPayloadValid() {
-                                checkapp.InfoPlistPath = checkapp.payloadPath.appendingPathComponent("\(checkapp.appNameInPayload)/Info.plist")
-                                if checkapp.checkInfoPlist() {
-                                    checkapp.getInfoPlistValue()
-                                    if checkapp.validInfoPlist {
-                                        print("valid InfoPlist")
-                                    } else {
-                                        checkapp.app_executable = nil
-                                        signFailedAlert(title: "No executable found.", message: "Missing executable in Info.plist")
-                                    }
+        VStack {
+            Text(checkapp.fileName != "" ? "\(checkapp.fileName)" : "No ipa file selected")
+            Button(action: {
+                if isImporting {
+                    isImporting = false
+                    DispatchQueue.main.asyncAfter(deadline: .now()+0.2, execute: {
+                        isImporting = true
+                    })
+                } else {
+                    isImporting = true
+                }
+            }, label: {Text("Select File")})
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text(alertTitle), message: Text(alertMeaasge), dismissButton: .default(Text("OK")))
+                }
+                .fileImporter(
+                    isPresented: $isImporting,
+                    allowedContentTypes: [UTType(filenameExtension: "ipa")!],
+                    allowsMultipleSelection: false
+                ) { result in
+                    do {
+                        let fileUrl = try result.get()
+                        checkapp.fileName = fileUrl.first!.lastPathComponent
+                        checkapp.filePath = fileUrl.first!.path
+                        
+                        checkapp.extractIpa()
+                        
+                        if checkapp.checkIsIpaPayloadValid() {
+                            checkapp.InfoPlistPath = checkapp.payloadPath.appendingPathComponent("\(checkapp.appNameInPayload)/Info.plist")
+                            if checkapp.checkInfoPlist() {
+                                checkapp.getInfoPlistValue()
+                                if checkapp.validInfoPlist {
+                                    print("valid InfoPlist")
+                                } else {
+                                    checkapp.app_executable = nil
+                                    signFailedAlert(title: "No executable found.", message: "Missing executable in Info.plist")
                                 }
-                            } else {
-                                signFailedAlert(title: "IPA is not valid!", message: "The file might have missing parts\nor invalid contents")
                             }
-                        } catch {
-                            print(error.localizedDescription)
+                        } else {
+                            signFailedAlert(title: "IPA is not valid!", message: "The file might have missing parts\nor invalid contents")
+                        }
+                        isImporting = false
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+                .padding()
+            
+            Button(action: {
+                DispatchQueue.global(qos: .userInitiated).async {
+                    progress.permanentSignButtonFunc()
+                    DispatchQueue.main.asyncAfter(deadline: .now()) {
+                        if progress.CheckDebBuild() {
+                            if checkFilza() {
+                                canShowinFilza = true
+                            } else { canShowinFilza = false }
+                            checkapp.fileName = ""
+                            showInFilzaAlert.toggle()
+                            
+                        } else {
+                            signFailedAlert(title: "Sign Failed", message: "Please try others iPA files")
                         }
                     }
-                    .padding()
-                
-                Button(action: {
-                    DispatchQueue.global(qos: .userInitiated).async {
-                        progress.permanentSignButtonFunc()
-                        finish()
-                    }
-                }, label: {Text("Permanent sign")})
-                .alert(isPresented: $showInFilzaAlert ){
-                    Alert(
-                        title: Text(canShowinFilza ? "Done" : "Ohh no"),
-                        message: Text(canShowinFilza ? "View the file now ?" : "You need Filza to view the file"),
-                        primaryButton: .default(Text("Okay")) {
-                            if canShowinFilza {
-                                UIApplication.shared.open(URL(string: "filza://\(progress.OutputDebFilePath)")!)
-                            }
-                        },
-                        secondaryButton: .cancel()
-                    )
                 }
-                .disabled(checkapp.fileName == "")
-                .opacity(checkapp.fileName == "" ? 0.6 : 1.0)
-                .buttonStyle(SignButtonStyle())
+            }, label: {Text("Permanent sign")})
+            .alert(isPresented: $showInFilzaAlert ){
+                Alert(
+                    title: Text(canShowinFilza ? "Done" : "Ohh no"),
+                    message: Text(canShowinFilza ? "View the file now ?" : "You need Filza to view the file"),
+                    primaryButton: .default(Text("Okay")) {
+                        if canShowinFilza {
+                            UIApplication.shared.open(URL(string: "filza://\(progress.OutputDebFilePath)")!)
+                        }
+                    },
+                    secondaryButton: .cancel()
+                )
             }
-            .padding()
+            .disabled(checkapp.fileName == "")
+            .opacity(checkapp.fileName == "" ? 0.6 : 1.0)
+            .buttonStyle(SignButtonStyle())
+            
+            if isImporting {
+                ProgressView(label: {
+                    Text("Importing iPA file")
+                })
+            }
+        }
+        .padding()
     }
 }
 
@@ -114,3 +126,4 @@ struct SignView_Previews: PreviewProvider {
         SignView()
     }
 }
+
