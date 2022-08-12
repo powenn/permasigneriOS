3c02fb2d76d93a5b953892dac29f3e1aa394aa4d
powenn <powen0966091716@gmail.com>
2022-08-12 12:08:36 +0800

    patch again :(

--- permasigneriOS/PackToDeb.swift
+++ permasigneriOS/PackToDeb.swift
@@ -132,7 +132,7 @@ class Progress: ObservableObject {
         // Scripts parts
         // /var/mobile/Documents/permasigneriOS/tmp/deb/DEBIAN/*
         AuxiliaryExecute.local.bash(command: "chmod 0755 \(DebDebianDirectory.path)/postrm")
-        AuxiliaryExecute.local.bash(command: "chmod 0755 \(DebDebianDirectory.path)/postinst)")
+        AuxiliaryExecute.local.bash(command: "chmod 0755 \(DebDebianDirectory.path)/postinst")
         
         // app_executable
         // /var/mobile/Documents/permasigneriOS/tmp/deb/Applications/ex.app/ex
